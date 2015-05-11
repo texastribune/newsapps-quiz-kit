8b469ce466d9bd1f1e1b81130e81b614f2bd2953
Becca Aaronson <becca.aaronson@gmail.com>
2015-05-11 14:54:04 -0500

    disable buttons after click, add tally counter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,16 +8,21 @@
   function render() {
 
     // Add your JS here!
+
+    var tally = 0;
+
     $('.response').click(function() {
       var question = $(this).parent();
 
       if ($(this).val() === '1') {
         $(question).find('.correct').show();
         $(question).find('.incorrect').hide();
+        tally++;
       } else {
         $(question).find('.incorrect').show();
         $(question).find('.correct').hide();
       }
+      $(question).find('.response').attr('disabled', true);
     });
 
     if (pymChild) {
