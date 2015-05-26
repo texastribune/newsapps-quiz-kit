316be7ff41cce17922bae2d8e9e743dd2a98882a
Becca Aaronson <becca.aaronson@gmail.com>
2015-05-26 10:32:38 -0500

    delete line to write data.json to assets folder

--- bin/fetch_spreadsheet.js
+++ bin/fetch_spreadsheet.js
@@ -129,7 +129,6 @@ function buildDataJSON(data) {
   });
 
   fs.writeFileSync('data.json', JSON.stringify(payload, null, 2));
-  fs.writeFileSync('app/assets/data/data.json', JSON.stringify(payload, null, 2));
 }
 
 primeToken(oauth2Client, function() {
